fddb025acebfeb5fc3b7fb8fb4a8884513b460bc
tbushman <tbushman@pu.bli.sh>
2019-01-20 14:53:41 -0700

    wip nock-record

--- probot/test/index.test.js
+++ probot/test/index.test.js
@@ -1,3 +1,5 @@
+const config = require('../../config');
+const expressJwt = require('express-jwt');
 const expect = require('expect');
 const nock = require('nock');
 const fetch = require('node-fetch');
@@ -6,18 +8,21 @@ const prOpened = require('./payloads/events/pullRequests.opened');
 const prExisting = require('./payloads/events/pullRequests.existing');
 const prUnrelated = require('./payloads/events/pullRequests.unrelated');
 const prClosed = require('./payloads/events/pullRequests.closed');
+const prDeleted = require('./payloads/events/pullRequests.deleted');
 const prOpenedFiles = require('./payloads/files/files.opened');
 const prExistingFiles = require('./payloads/files/files.existing');
 const prUnrelatedFiles = require('./payloads/files/files.unrelated');
 const probotPlugin = require('..');
 const { PRtest } = require('./utils/testmodels');
 const { setupRecorder } = require('nock-record');
+const fs = require('fs').promises;
+const path = require('path');
 
 const record = setupRecorder();
 // const mongoose = require('mongoose');
 
 describe('Presolver', () => {
-  let probot, github, owner, repo;
+  let probot, github, owner, repo, app;
 
   afterEach(async (done) => {
     await PRtest.deleteMany({}).catch(err => console.log(err));
@@ -28,13 +33,17 @@ describe('Presolver', () => {
 
     probot = new Probot({});
     // Load our app into probot
-    let app = await probot.load(probotPlugin);
+    app = await probot.load(probotPlugin);
     await PRtest.deleteMany({}).catch(err => console.log(err));
     // This is an easy way to mock out the GitHub API
     // https://probot.github.io/docs/testing/
     owner = 'freeCodeCamp-rocks';
     repo = 'freeCodeCamp';
-
+    // const githubAuth = {
+    //   type: 'basic',
+    //   username: config.github.probot.clientId,
+    //   password: config.github.probot.clientSecret
+    // };
     github = {
       issues: {
         /* eslint-disable no-undef */
@@ -84,27 +93,45 @@ describe('Presolver', () => {
       }
     };
     app.auth = () => Promise.resolve(github);
+    app.on('pull_request.deleted', recordContext.bind(app));
+    async function recordContext(data) {
+      const dir = path.join(__dirname, '..', config.github.probot.privateKeyId);
+      const k = await fs.readFile(dir).then((data) => data);
+      console.log(k);
+      const { completeRecording } = await record('pr-stats');
+      const github = await app.auth(config.github.probot.clientId, k);
+      // const context = data;
+      completeRecording();
+      console.log(github);
+      expect(github).not.toBe(null);
+    }
   });
 
   test('should receive repo info', async() => {
-    const { completeRecording } = await record('pr-stats');
+    await probot.receive({
+      name: 'pull_request',
+      payload: prDeleted
+    });
+    /* const { completeRecording } = await record('pr-stats');
     // const result = // getState.bind(app);
-    fetch(`https://api.github.com/search/issues
-        ?q=repo:${owner}/${repo}+is:open+type:pr+base:master&
-        sort=created&order=asc&page=1&per_page=1`)
+    const data =
+    // await fetch(
+    `https://api.github.com/search/issues?
+    q=repo:${owner}/${repo}+is:open+type:pr+base:master&
+    sort=created&order=asc&page=1&per_page=1`)
     // await octokit.search.issues({
         // q: `repo:${owner}/${repo}+is:open+type:pr+base:master`,
         // sort: 'created', order: 'asc', page: 1, per_page: 1
       // })
-      .then((data) => {
-        completeRecording();
-        console.log(data);
-        expect(data).not.toBe(null);
-      })
+      .then((data) => data)
       .catch(err => console.log(err));
-
+      completeRecording();
+      console.log(data);
+      expect(data).not.toBe(null);
+*/
+console.log(app)
   });
-
+/*
   test(`adds a label if a PR has changes to files targeted by an
     existing PR`, async () => {
     // Receive a webhook event
@@ -142,7 +169,7 @@ describe('Presolver', () => {
       .catch(err => console.log(err));
     expect(result).toBe(null);
 
-  });
+  }); */
 });
 
 // For more information about testing with Jest see:
